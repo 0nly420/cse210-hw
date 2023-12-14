51d597694bf0b300a5ce102a8d582f2399d3b42e
0nly420 <131375398+0nly420@users.noreply.github.com>
2023-12-14 00:29:25 -0500

    Update Program.cs

--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -1,9 +1,88 @@
-using System;
 
-class Program
+public class Product
 {
-    static void Main(string[] args)
+
+    private string name;
+    private double price;
+    private int quantityInStock;
+
+
+    public Product(string name, double price, int initialQuantity)
     {
-        Console.WriteLine("Hello FinalProject World!");
+        this.name = name;
+        this.price = price;
+        this.quantityInStock = initialQuantity;
     }
+
+
+    public void UpdatePrice(double newPrice)
+    {
+        this.price = newPrice;
+    }
+
+
+}
+
+public class Inventory
+{
+
+    private List<Product> products;
+
+
+    public Inventory()
+    {
+        this.products = new List<Product>();
+    }
+
+
+    public void AddProduct(Product newProduct)
+    {
+        this.products.Add(newProduct);
+    }
+
+
+}
+
+
+public class Sales
+{
+
+    private Inventory inventory;
+
+
+    public Sales(Inventory inventory)
+    {
+        this.inventory = inventory;
+    }
+
+
+    public void RecordSale(Product product, int quantitySold)
+    {
+
+    }
+
+
+}
+
+
+public class UserInterface
+{
+
+    private Inventory inventory;
+    private Sales salesSystem;
+
+
+    public UserInterface(Inventory inventory, Sales salesSystem)
+    {
+        this.inventory = inventory;
+        this.salesSystem = salesSystem;
+    }
+
+
+    public void ShowMainMenu()
+    {
+
+    }
+
+
 }
